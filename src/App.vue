f838e531cdd96f51e64d9e8ba52c425d9e1c81aa
Aileen Rae <aileen@stormideas.com>
2019-03-15 17:09:28 +0000

    Using bootstrap-vue alert component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-      App loaded with Bootstrap!
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
+    <b-alert variant="success" show dismissible>App loaded with Bootstrap!</b-alert>
     <b-container>
       <router-view/>
     </b-container>
@@ -17,7 +12,9 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+.alert {
+  text-align: center;
 }
 </style>
